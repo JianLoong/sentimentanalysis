bd8a2574f940f85d175824d40b5a03f42d73f834
Jian Liew <jianloongliew@gmail.com>
2022-10-22 16:44:31 +1100

    Updated UI

--- worker.js
+++ worker.js
@@ -7,7 +7,7 @@ if (window.Worker) {
     const result = document.querySelector(".result");
     const entry = document.querySelector("#inputString");
     const textArea = document.getElementById("inputString");
-
+    
     const selectedAlgorithm = document.getElementById("selectedAlgorithm");
 
     const vaderWorker = new Worker("vader-worker.js");
@@ -41,12 +41,29 @@ if (window.Worker) {
 const processAfinn = (worker, entry, result) => {
     console.log("In afinn")
 
+    const verdict = document.querySelector(".verdict");
 
-    if (entry.value == "")
+    if (entry.value == ""){
+        verdict.innerHTML = "";
+        result.innerHTML = "";
         return;
+    }
     worker.postMessage(entry.value);
 
     worker.onmessage = function (e) {
+
+        if(e.data < 0 ){
+            verdict.innerHTML = '<div class="alert alert-warning" role="alert">Negative</div>';
+        }
+
+        if(e.data == 0 ){
+            verdict.innerHTML = '<div class="alert alert-info" role="alert">Neutral</div>';
+        }
+
+        if(e.data > 0 ){
+            verdict.innerHTML = '<div class="alert alert-success" role="alert">Positive</div>';
+        }
+
         let tableHTML = '<table class="result-table table table-bordered table-striped mb-0"><thead><tr><th>Score</th></thead>';
 
         console.log(e.data)
@@ -58,14 +75,44 @@ const processAfinn = (worker, entry, result) => {
 
 const processVader = (worker, entry, result) => {
 
+    const verdict = document.querySelector(".verdict");
+
     console.log("In vader")
-    if (entry.value == "")
+
+    if (entry.value == ""){
+        verdict.innerHTML = "";
+        result.innerHTML = "";
         return;
+    }
     console.log("posting to vader worker");
     worker.postMessage(entry.value);
 
     worker.onmessage = function (e) {
         console.log("returned from worker");
+
+       
+
+        if(e.data.compound <= 0.05 ){
+            verdict.innerHTML = '<div class="alert alert-warning" role="alert">Negative</div>';
+        }
+
+        if(e.data.compound > -0.05 && e.data.compound < 0.05 ){
+            verdict.innerHTML = '<div class="alert alert-info" role="alert">Neutral</div>';
+        }
+
+        if(e.data.compound >= 0.05 ){
+            verdict.innerHTML = '<div class="alert alert-success" role="alert">Positive</div>';
+        }
+
+        if(e.data.compound >= 1 ){
+            verdict.innerHTML = '<div class="alert alert-success" role="alert">Most extreme positive</div>';
+        }
+
+        if(e.data.compound <= -1 ){
+            verdict.innerHTML = '<div class="alert alert-warning" role="alert">Most extreme negative</div>';
+        }
+
+
         let tableHTML = '<table class="result-table table table-bordered table-striped mb-0"><thead><tr><th>Negative</th><th>Positive</th><th>Compound</th></tr></thead>';
 
         result.innerHTML = tableHTML + "<tr><td>" + e.data.neg + "</td><td>" + e.data.pos + "</td><td>" + e.data.compound + "</td></tr></table>";
